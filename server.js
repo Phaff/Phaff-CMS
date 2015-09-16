b3dc82a3f8541ac4e2186739c98e8a2e41124c7d
Gaël Demette <demette.gael@gmail.com>
2015-09-16 15:13:21 +0200

    Poc for object

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 
 const http = require('http');
 
-const Phaff = require('phaff');
+const Counter = require('phaff');
 
+var counter = new Counter(1);
 var server = http.createServer((req, res) => {
+    counter.increment();
+    var val = counter.get();
     res.writeHead(200, {});
-    res.write(Phaff.hello_world());
+    res.write(val + "");
     res.end();
 });
 server.listen(1337);
