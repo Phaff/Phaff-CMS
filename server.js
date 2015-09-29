a207c861c7a8e7f49027bd5b3f77074638cec878
Gaël Demette <demette.gael@gmail.com>
2015-09-30 00:18:50 +0200

    get service webserver

--- server.js
+++ server.js
@@ -7,22 +7,30 @@ const Phaff = require('phaff');
 
 let phaff = new Phaff();
 
-
-phaff
-    .readFrameworkConfig()
-    .then(
-        () => phaff.config.readDirectory(path.join(__dirname, 'config'))
+Promise
+    .all(
+        [
+            phaff.initConfig(),
+            phaff.initServices()
+        ]
     )
     .then(
-        () => phaff.newServer()
+        //() => phaff.config.readDirectory(path.join(__dirname, 'config'))
+    )
+    .then(
+        () => phaff.getService('phaff/webserver')
     )
     .then(
         (server) => {
+            console.log(server);
             fs.readdir(path.join(__dirname, 'controllers'), (err, files) => {
                 files.forEach((file) => {
                     server.registerRoute(require(path.join(__dirname, 'controllers', file)));
                 });
                 server.listen(1337);
             });
+        },
+        (error) => {
+            console.log(error);
         }
     );
