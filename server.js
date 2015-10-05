120720efb872355661da4200e7d000142fc3d9c9
Gaël Demette <demette.gael@gmail.com>
2015-10-06 00:57:06 +0200

    working init, must be simplified

--- server.js
+++ server.js
@@ -7,30 +7,29 @@ const Phaff = require('phaff');
 
 let phaff = new Phaff();
 
-Promise
-    .all(
-        [
-            phaff.initConfig(),
-            phaff.initServices()
-        ]
+phaff.initServices()
+    .then(
+        () => phaff.initConfig()
     )
     .then(
-        //() => phaff.config.readDirectory(path.join(__dirname, 'config'))
+        () => phaff.getService('Phaff/Config')
     )
     .then(
-        () => phaff.getService('phaff/webserver')
+        (config) => config.readDirectory(path.join(__dirname, 'config'))
+    )
+    .then(
+        (config) => phaff.getService('Phaff/Config')
+    )
+    .then(
+        () => phaff.getService('Phaff/WebServer')
     )
     .then(
         (server) => {
-            console.log(server);
             fs.readdir(path.join(__dirname, 'controllers'), (err, files) => {
                 files.forEach((file) => {
                     server.registerRoute(require(path.join(__dirname, 'controllers', file)));
                 });
                 server.listen(1337);
             });
-        },
-        (error) => {
-            console.log(error);
         }
     );
